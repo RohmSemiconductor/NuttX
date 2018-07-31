b1915a7ed90eadcdde7b00b83d0448e5057dc80d
jutava <jutava@hotmail.fi>
2018-07-31 10:27:24 +0300

    Update bh1749nuc_scu.c

--- sdk/drivers/sensors/bh1749nuc_scu.c
+++ sdk/drivers/sensors/bh1749nuc_scu.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * drivers/sensors/bh1749nuc_scu.c
  *
- *   Copyright (C) 2018 Sony Corporation
+ *   Copyright (C) 2018 ROHM Co.,Ltd.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
